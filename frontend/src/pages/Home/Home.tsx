4b2c5a13c00af809d981207ffaf5bd738ae4adbe
Mark <3188560574@qq.com>
2026-06-06 00:45:20 +0000

    welcome page done

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import Marquee from './Marquee'
+import TopNav from './TopNav'
+import BottomStage from './BottomStage'
+
+function Home() {
+  return (
+    <div className="home-page">
+      <TopNav />
+      <Marquee />
+      <BottomStage />
+    </div>
+  )
+}
+
+export default Home
