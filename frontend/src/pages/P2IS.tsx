7752ab151e4398d43de313a9582c7aff6945cc68
Mark <3188560574@qq.com>
2026-06-08 20:05:04 +0000

    refactor: restructure backend to Node.js, move P2IS to directory

--- frontend/src/pages/P2IS.tsx
+++
@@ -1,5 +0,0 @@
-function P2IS() {
-    return <div>PSIS 页面</div>
-}
-
-export default P2IS
---
+++ frontend/src/pages/P2IS/P2IS.tsx
@@ -0,0 +1,12 @@
+import TopNav from "../Home/TopNav.tsx"
+
+function P2IS() {
+    return <div className="p2is-page">
+      <div id="p2is-links">
+        <a href="https://pan.baidu.com/s/14s1VWYrj6qWDelHmEIpQ7Q?pwd=ij8t">百度网盘</a>
+        <a href="https://github.com/Mark-lu-051030/P2IS_Translation_Tools/releases/tag/P2IS%E6%B1%89%E5%8C%96%E5%85%AC%E6%B5%8B%E7%89%88V0.1.0">Github</a>
+      </div>
+    </div>
+}
+
+export default P2IS
